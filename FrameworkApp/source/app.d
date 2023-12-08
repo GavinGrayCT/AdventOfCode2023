ec2f2e406b7d7fae5e760584a022d9848e7bd966
gavin <gavin.gray@remss.net>
2023-12-08 14:08:26 +0200

    Framework as start for each day.

---
+++ FrameworkApp/source/app.d
@@ -0,0 +1,36 @@
+import std.stdio;
+import core.time;
+import std.conv;
+import std.format;
+import std.file;
+import std.algorithm;
+import std.ascii;
+
+void main()
+{
+  string answerTextPart1 = "Day x, Part 1:";
+  string answerTextPart2 = "Day x, Part 2:";
+  ulong answerPart1 = 0;
+  ulong answerPart2 = 0;
+  auto startTime = MonoTime.currTime;
+  auto pathFilename = "data/thedata.txt";
+  string data = cast(string)read(pathFilename);
+
+  foreach (word; splitter(data)) {
+    extractData(word);
+  }
+
+  // Debug
+
+
+  auto endTime = MonoTime.currTime;
+  auto duration = endTime - startTime;
+  writefln("Calc duration ==> %s usecs", duration.total!"usecs");
+  writeln(format!"%s %s"(answerTextPart1, answerPart1));
+  writeln(format!"%s %s"(answerTextPart2, answerPart2));
+}
+
+void extractData(string word) {
+  writeln(format!"Word is: %s"(word));
+}
+
