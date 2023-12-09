2ef9b5d835fe594f1a3aa52e9cc1db5bd8d8d2f1
gavin <gavin.gray@remss.net>
2023-12-09 14:32:11 +0200

    Day 8 Part 2 wrong answer - too big

--- DayHauntedWasteLand/source/app.d
+++ DayHauntedWasteLand/source/app.d
@@ -5,6 +5,7 @@ import std.format;
 import std.file;
 import std.algorithm;
 import std.ascii;
+import std.bigint;
 
 char[] directives;
 string[][string] nodes;
@@ -14,7 +15,7 @@ void main()
   string answerTextPart1 = "Day x, Part 1:";
   string answerTextPart2 = "Day x, Part 2:";
   ulong answerPart1 = 0;
-  ulong answerPart2 = 0;
+  BigInt answerPart2 = 1;
   auto startTime = MonoTime.currTime;
   auto pathFilename = "data/thedata.txt";
   string data = cast(string)read(pathFilename);
@@ -29,36 +30,80 @@ void main()
   }
 
   writeln(format!"directives.length: %s"(directives.length));
-    nodeName = "AAA";
-    ulong steps = 0;
-    ulong index = 0;
-    for (int i = 0; ; i++) {
-      index = i % directives.length;
-      steps += 1;
-      string[]aLeftRight = nodes[nodeName];
-      writeln(format!"i: %s, nodeName: %s, directive: %s, left: %s, right: %s"(i, nodeName, directives[index], aLeftRight[0], aLeftRight[1]));
+  // Get initial nodes name
+  string[] nodesName;
+  foreach(name; nodes.keys) {
+    if (name[2] == 'A') {
+      nodesName ~= name;
+    }
+  }
+
+  writeln(format!"Starting nodes: %s"(nodesName));
+
+  ulong step = 0;
+  ulong index = 0;
+  ulong[] lastZStep;
+  ulong[] intervals;
+  lastZStep.length = nodesName.length;
+  intervals.length = nodesName.length;
+  for (ulong i = 0; ; i++) {
+    index = i % directives.length;
+    step += 1;
+    bool allEndInZ = false;
+    // writeln(format!"Step: %s, node names: %s"(step, nodesName));
+   for (ulong j = 0; j < nodesName.length; j++) {
+      string name = to!string(nodesName[j]);
+      //writeln(format!"nodesName[0]: %s, name: %s"(nodesName[0], name));
+      string[]aLeftRight = nodes[name];
+      // writeln(format!"i: %s, j: %s, name: %s, directive: %s, left: %s, right: %s"(i, j, name, directives[index], aLeftRight[0], aLeftRight[1]));
       if (directives[index] == 'L') {
-        nodeName = aLeftRight[0];
+        nodesName[j] = aLeftRight[0];
       } else if (directives[index] == 'R') {
-        nodeName = aLeftRight[1];
+        nodesName[j] = aLeftRight[1];
       } else {
         throw new Exception(format!"Expected L or R, got: |%s|"(directives[index]));
       }
-      if (nodeName == "ZZZ") {
-        break;
-      }
-      if (steps > 1000000) {
-        throw new Exception(format!"Probably not going to finish");
+      if (nodesName[j][2] == 'Z') {
+        intervals[j] = step - lastZStep[j];
+        lastZStep[j] = step;
+        writeln(format!"Got --Z in: %s, at step: %s, interval: %s, j: %s"(nodesName[j], step, intervals[j], j));
+        // allEndInZ = false;
+        // break;
       }
     }
-    answerPart1 = steps;
-
-    auto endTime = MonoTime.currTime;
-    auto duration = endTime - startTime;
-    writefln("Calc duration ==> %s usecs", duration.total!"usecs");
-    writeln(format!"%s %s"(answerTextPart1, answerPart1));
-    writeln(format!"%s %s"(answerTextPart2, answerPart2));
+    if (allEndInZ) {
+      break;
+    }
+    if (step > 100_000) { // 14_321_394_058_031 JG
+      //throw new Exception(format!"Probably not going to finish. Staps: %s"(step));
+      break;
+    }
   }
+  writeln(format!"Intervals %s"(intervals));
+  ulong gcdIntervals = gcdOfArray(intervals);
+  writeln("The GCD of the array is: ", gcdIntervals);
+  ulong[] remainders;
+  foreach(interval; intervals) {
+    remainders ~= interval % gcdIntervals;
+  }
+
+  BigInt productIntervals = 1;
+  foreach(i, a; intervals) {
+    if (i == 0) {
+      a = a / gcdIntervals;
+    }
+    productIntervals *= a;
+  }
+  answerPart2 = productIntervals;
+  writeln(format!"productIntervals: %s, gcd: %s, answerPart2: %s, ulong.max: %s"(productIntervals, gcdIntervals, answerPart2, ulong.max));
+  writeln(format!"Remainders: %(%s %)"(remainders));
+
+  auto endTime = MonoTime.currTime;
+  auto duration = endTime - startTime;
+  writefln("Calc duration ==> %s usecs", duration.total!"usecs");
+  writeln(format!"%s %s"(answerTextPart1, answerPart1));
+  writeln(format!"%s %s"(answerTextPart2, answerPart2));
+}
 
 string state = "Get Directives";
 string nodeName = "";
@@ -97,4 +142,36 @@ void extractData(string word) {
     }
   }
 }
+
+import std.stdio;
+import std.algorithm;
+import std.array;
+
+// Function to find the GCD of two numbers
+ulong gcd(ulong a, ulong b) {
+    while (b != 0) {
+        ulong t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+// Function to find the GCD of an array of ulongs
+ulong gcdOfArray(ulong[] arr) {
+    ulong result = arr[0];
+    foreach (num; arr[1 .. $]) {
+        result = gcd(result, num);
+        if (result == 1) {
+            return 1; // Early exit if GCD becomes 1
+        }
+    }
+    return result;
+}
+
+// void main() {
+//     // Example usage
+//     ulong[] numbers = [24, 60, 36]; // Replace with your set of ulongs
+//     writeln("The GCD of the array is: ", gcdOfArray(numbers));
+// }
 
